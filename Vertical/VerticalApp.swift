4dc36c7387be47f5cb528e55941cdcfbbaf00902
Ethan Patrick West <ethan.west@uvm.edu>
2022-10-13 10:51:00 -0400

    Initial Commit

---
+++ Vertical/VerticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalApp.swift
+//  Vertical
+//
+//  Created by Ethan West on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct VerticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

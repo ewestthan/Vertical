0923baec20d066ad4881ee9c1c6a175f217805a8
Ethan Patrick West <ethan.west@uvm.edu>
2022-10-18 19:40:26 -0400

    First Commit

---
+++ Vertical/TaskBar.swift
@@ -0,0 +1,21 @@
+//
+//  TaskBar.swift
+//  Vertical
+//
+//  Created by Ethan West on 10/18/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct TaskBar: View {
+    
+    var body: some View {
+        HStack{
+            Text("Home")
+            Text("User Profile")
+            Text("Area Profile")
+            Text("Climb Profile")
+        }
+    }
+}
---
+++ Vertical/UserProfileHeader.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileHeader.swift
+//  Vertical
+//
+//  Created by Ethan West on 10/18/22.
+//
+import SwiftUI
+import Foundation
+
+struct UserProfileHeader: View {
+    
+    var body: some View {
+        VStack{
+            Text("Username")
+            Text("@tag")
+        }
+    }
+}

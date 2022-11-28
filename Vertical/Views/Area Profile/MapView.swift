bfbe94670a5d696cbaf39d94abbce69927bc0daf
Ethan Patrick West <ethan.west@uvm.edu>
2022-11-28 15:43:28 -0500

    Started making Map dynamic

--- Vertical/Views/Area Profile/MapView.swift
+++ Vertical/Views/Area Profile/MapView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 44.4778528, longitude: -73.1964637),
+    var area: Area
+
+    lazy private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: area.xCoord, longitude: area.yCoord),
         span: MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
     )
 
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: self.$region)
     }
 }
 
@@ -45,8 +46,8 @@ struct PinAnnotationMapView: View {
     }
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
-}
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView(area: constant()
+//    }
+//}

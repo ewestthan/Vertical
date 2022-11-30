e6f87d4a6fc39784cfb2a990ef357281c26cf85a
mlalongi <matthew.alongi@uvm.edu>
2022-11-29 19:20:34 -0500

    new map view

--- Vertical/Views/Area Profile/MapView.swift
+++ Vertical/Views/Area Profile/MapView.swift
@@ -1,5 +1,5 @@
 //
-//   MapView.swift
+//  MapView.swift
 //  Vertical
 //
 //  Created by Matthew Alongi on 11/15/22.
@@ -9,45 +9,19 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    var area: Area
-
-    lazy private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: area.xCoord, longitude: area.yCoord),
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 44.4778528, longitude: -73.1964637),
         span: MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
     )
-
+    
     var body: some View {
-        Map(coordinateRegion: self.$region)
+        Map(coordinateRegion: $region)
     }
 }
 
-struct IdentifiablePlace: Identifiable {
-    let id: UUID
-    let location: CLLocationCoordinate2D
-    init(id: UUID = UUID(), lat: Double, long: Double) {
-        self.id = id
-        self.location = CLLocationCoordinate2D(
-            latitude: 44.4778528,
-            longitude: -73.1964637)
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
     }
 }
-
-struct PinAnnotationMapView: View {
-    let place: IdentifiablePlace
-    @State var region: MKCoordinateRegion
-
-    var body: some View {
-        Map(coordinateRegion: $region,
-            annotationItems: [place])
-        { place in
-            MapMarker(coordinate: place.location,
-                   tint: Color.purple)
-        }
-    }
-}
-
-//struct MapView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MapView(area: constant()
-//    }
-//}

a8879bf8944fa3e31a157a0d10b602b198ac0355
Shawn Rodgers <me@directedbyshawn.com>
2022-12-06 15:31:20 -0500

    cleaned up a bit

--- Vertical/ViewModel/AreaViewModel.swift
+++ Vertical/ViewModel/AreaViewModel.swift
@@ -21,7 +21,6 @@ class AreaViewModel: ObservableObject {
     
     init(area: Area) {
         self.area = area
-        //self.areaClimbs = [AreaClimb(id: "EBSczr1BNlIimiAXUNT5", name: "Primate", rank: 5), AreaClimb(id: "cr50dO13IAczdhosJGQZ", name: "The Masochist", rank: 3), AreaClimb(id: "WU1KgVjqpaljL8ei1DVK", name: "Pac Man", rank: 5), AreaClimb(id: "W9nb7XnZt0ontEAV9rFi", name: "The Impossible Problem", rank: 3)]
         self.areaClimbs = [AreaClimb]()
         self.climb = ClimbProfileModel(Name: "test", Grade: 4, Rating: 4, Area: "Test", Picture_URL: "Test")
     }
@@ -61,6 +60,7 @@ class AreaViewModel: ObservableObject {
         
         var climbs = [AreaClimb]()
         var climb = AreaClimb()
+        
         let snapshot = try await AREAS_COLLECTION.document(areaID).collection("climbs").getDocuments()
         snapshot.documents.forEach { docSnap in
             let data = docSnap.data()
@@ -70,23 +70,9 @@ class AreaViewModel: ObservableObject {
             climb = AreaClimb(id: id, name: name, rank: rank)
             climbs.append(climb)
         }
+        
         return climbs
+        
     }
         
-        
-        
-//        AREAS_COLLECTION.document(areaID).collection("climbs").getDocuments { (querySnapshot, error) in
-//            guard let documents = querySnapshot?.documents else {
-//                print("no climbs")
-//                return
-//            }
-//            self.areaClimbs = documents.map { (queryDocumentSnapshot) -> AreaClimb in
-//                let data = queryDocumentSnapshot.data()
-//                let id = data["id"] as? String ?? "None"
-//                let name = data["name"] as? String ?? "None"
-//                let rank = data["rank"] as? Int ?? 0
-//                return AreaClimb(id: id, name: name, rank: rank)
-//            }
-//        }
-    
 }
